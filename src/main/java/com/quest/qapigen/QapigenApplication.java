32327e22f750e2230813ce958c0213e071f6a8dd
Rupesh Mhatre <rupesh.mhatre@hp.com>
2023-06-26 14:06:13 +0530

    initial commit

---
+++ src/main/java/com/quest/qapigen/QapigenApplication.java
@@ -0,0 +1,13 @@
+package com.quest.qapigen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QapigenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QapigenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/quest/qapigen/QapigenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quest.qapigen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QapigenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
