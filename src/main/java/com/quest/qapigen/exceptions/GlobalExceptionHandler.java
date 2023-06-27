26a452fcdb3d4ede87d7920ff350b9e3c5e6ecb6
rohit-ranjan <rohit.ranjan@hp.com>
2023-06-27 12:35:35 +0530

    Removed unused import

--- src/main/java/com/quest/qapigen/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/quest/qapigen/exceptions/GlobalExceptionHandler.java
@@ -1,6 +1,5 @@
 package com.quest.qapigen.exceptions;
 
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
